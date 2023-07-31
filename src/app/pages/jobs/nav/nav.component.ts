42bc5d916addc6aa54294f26bb302ac4e9841133
tnav97 <naveen.t@interspark.com>
2023-07-31 12:31:04 +0530

    host listener code

--- src/app/pages/jobs/nav/nav.component.ts
+++ src/app/pages/jobs/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 
 @Component({
@@ -8,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class NavComponent {
  isOpen = false;
+
+ @HostListener('document:click', ['$event'])
+ onDocumentClick(event: Event) {
+  const targetElement = event.target as HTMLElement;
+  const svgElement = document.getElementById('svg-data');
+  const svgElement2 = document.getElementById('svg-data2');
+   if (targetElement !== svgElement && targetElement !== svgElement2) {
+     // Close the menu when clicking outside of it
+     this.isOpen=false;
+   }
+ }
 }
